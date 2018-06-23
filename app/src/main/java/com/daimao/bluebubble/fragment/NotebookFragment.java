eb64606c65436436536786e6ed546463d3638b4f
bai-ge <13077790403@163.com>
2018-06-24 02:38:15 +0800

    Initial commit

---
+++ app/src/main/java/com/daimao/bluebubble/fragment/NotebookFragment.java
@@ -0,0 +1,126 @@
+package com.daimao.bluebubble.fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
+
+import com.daimao.bluebubble.BaseApplication;
+import com.daimao.bluebubble.R;
+import com.daimao.bluebubble.adapter.NoteAdapter;
+import com.daimao.bluebubble.model.NoteEntity;
+import com.etsy.android.grid.StaggeredGridView;
+import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import cn.droidlover.xdroidmvp.mvp.XFragment;
+
+/**
+ * Created by baige on 2018/6/18.
+ */
+
+public class NotebookFragment extends XFragment {
+
+    @BindView(R.id.search_box)
+    SearchBox mSearchBox;
+
+    @BindView(R.id.grid_view)
+    StaggeredGridView mGridView;
+
+    private NoteAdapter mAdapter;
+
+    private ArrayList<NoteEntity> mNoteList = new ArrayList<>();
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+        initSearchBox();
+        initNotebook();
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.frag_notebook;
+    }
+
+    @Override
+    public Object newP() {
+        return null;
+    }
+
+    public static NotebookFragment newInstance() {
+        return new NotebookFragment();
+    }
+
+    private void initSearchBox(){
+        mSearchBox.enableVoiceRecognition(this);
+        for(int x = 0; x < 10; x++){
+            SearchResult option = new SearchResult("Result " + Integer.toString(x), getResources().getDrawable(R.drawable.ic_history));
+            mSearchBox.addSearchable(option);
+        }
+        mSearchBox.setMenuListener(new SearchBox.MenuListener() {
+            @Override
+            public void onMenuClick() {
+                BaseApplication.getInstance().showTip("菜单");
+            }
+        });
+        mSearchBox.setSearchListener(new SearchBox.SearchListener(){
+
+            @Override
+            public void onSearchOpened() {
+
+            }
+
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+            @Override
+            public void onSearchClosed() {
+
+            }
+
+            @Override
+            public void onSearchTermChanged(String s) {
+
+            }
+
+            @Override
+            public void onSearch(String s) {
+
+            }
+
+            @Override
+            public void onResultClick(SearchResult searchResult) {
+
+            }
+        });
+
+        mSearchBox.setOverflowMenu(R.menu.overflow_menu);
+        mSearchBox.setOverflowMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.test_menu_item:
+                        BaseApplication.getInstance().showTip("onClick");
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initNotebook(){
+        mAdapter = new NoteAdapter(getContext(), R.layout.item_note);
+        for (int i = 0; i < 10; i++) {
+            NoteEntity note = new NoteEntity();
+            note.setNoteTitle("标题"+i);
+            note.setNoteContent("内容"+i);
+            mNoteList.add(note);
+        }
+        mAdapter.addAll(mNoteList);
+        mGridView.setAdapter(mAdapter);
+    }
+}
